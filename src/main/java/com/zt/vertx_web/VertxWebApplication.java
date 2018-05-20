e6f1700f423261dd28ea3acae18ac92ebfa574c3
zhoutao <619598856@qq.com>
2018-05-20 13:28:37 +0800

    Initial commit

---
+++ src/main/java/com/zt/vertx_web/VertxWebApplication.java
@@ -0,0 +1,12 @@
+package com.zt.vertx_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VertxWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VertxWebApplication.class, args);
+	}
+}
---
+++ src/test/java/com/zt/vertx_web/VertxWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zt.vertx_web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VertxWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
